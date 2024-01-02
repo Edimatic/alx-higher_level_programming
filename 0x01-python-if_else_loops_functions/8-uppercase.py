c25e53fac52e7d94507db2dd0e9269004cab7e4b
Edimatic <ediomiudofia@gmail.com>
2024-01-02 14:16:32 -0800

    Python project on if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
