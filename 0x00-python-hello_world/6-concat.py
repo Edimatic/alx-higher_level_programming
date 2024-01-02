7213b0b6aa1c0cb484292696575a693cb09af02d
Edimatic <ediomiudofia@gmail.com>
2024-01-02 13:16:14 -0800

    Python project work

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+combined_str = str1 + " " + str2
+print(f"Welcome to {combined_str}!")
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import this
+
+import this
+
+import this 
+
+import this `
