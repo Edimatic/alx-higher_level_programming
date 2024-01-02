8f262624a5e0ac64eb6ea0f3426b7ed0ee3db32b
Edimatic <ediomiudofia@gmail.com>
2024-01-02 14:43:20 -0800

    program that prints the ASCII

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
+    if chr(i) not in ['e', 'q']:
         print('{:c}'.format(i), end='')
+
+print()
