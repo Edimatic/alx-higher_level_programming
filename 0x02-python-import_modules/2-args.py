35e92857b728510004ba516ad338a5a6c08238cd
Edimatic <ediomiudofia@gmail.com>
2024-01-04 04:16:03 -0800

    make a script dynamic

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for x in range(count):
+        print("{}: {}".format(x + 1, sys.argv[x + 1]))
