9b54115d0c898a78db1c45d9449c98f20964b58d
Edimatic <ediomiudofia@gmail.com>
2024-01-04 04:27:17 -0800

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-    for v in range(len(sys.argv) - 1):
-        total += int(sys.argv[v + 1])
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
     print("{}".format(total))
