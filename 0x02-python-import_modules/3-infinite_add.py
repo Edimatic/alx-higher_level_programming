ca4e9af1ab3fc2599b233f0e80eaab4ef1fbeb37
Edimatic <ediomiudofia@gmail.com>
2024-01-04 04:23:14 -0800

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!usr/bin/python
+
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for v in range(len(sys.argv) - 1):
+        total += int(sys.argv[v + 1])
+    print("{}".format(total))
