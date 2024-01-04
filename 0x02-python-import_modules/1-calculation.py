b4a964e19b271d164cbfaf5c163db8d67642df17
Edimatic <ediomiudofia@gmail.com>
2024-01-04 04:08:56 -0800

    My first toolbox

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-# Content: def add(a, b), def subtract(a, b), def multiply(a, b), def divide(a, b)
 
+if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, subtract, multiply, divide
 
-# Main program
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-# Importing functions from calculator_1.py
-from calculator_1 import add, subtract, multiply, divide
-
-# Performing calculations and printing results
-result_add = add(a, b)
-result_subtract = subtract(a, b)
-result_multiply = multiply(a, b)
-result_divide = divide(a, b)
-
-# Printing results with a single print statement
-print("{} + {} = {}".format(a, b, result_add))
-print("{} - {} = {}".format(a, b, result_subtract))
-print("{} * {} = {}".format(a, b, result_multiply))
-print("{} / {} = {}".format(a, b, result_divide))
-
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, subtract(a, b)))
+    print("{} * {} = {}".format(a, b, multiply(a, b)))
+    print("{} / {} = {}".format(a, b, divide(a, b)))
