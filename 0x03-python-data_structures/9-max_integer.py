f3a1e6b19a92853cf56f5da81e3848b55cc4de4e
Edimatic <ediomiudofia@gmail.com>
2024-01-07 15:54:14 -0800

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    max_value = my_list[0]
+
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+
+    return max_value
