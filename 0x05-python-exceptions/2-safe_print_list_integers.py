6e7712cdab3a68ea332fb2ea706e7f901fb66ebc
Edimatic <ediomiudofia@gmail.com>
2024-01-22 15:01:33 -0800

    Print and check integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,21 @@
+def safe_print_list_integers(my_list=[], x=0):
+    """Print the first x elements of a list that are integers.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+        checked = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end="")
+                checked += 1
+        print()
+    except IndexError:
+        print()
+    finally:
+        return checked
