905c06fe35d67ee3ef02ac3c8cd338a830cd9ade
Edimatic <ediomiudofia@gmail.com>
2024-02-06 06:43:27 -0800

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Defines a text file-reading function."""
-
 
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    :param filename: The name of the file to be read.
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
