f90eca8337f2fda9a04bbbdad18bd2a63be9d868
Edimatic <ediomiudofia@gmail.com>
2024-02-22 15:42:06 -0800

    python circle project

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,7 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """Represent a square."""
+
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize start up."""
         self.size = size
