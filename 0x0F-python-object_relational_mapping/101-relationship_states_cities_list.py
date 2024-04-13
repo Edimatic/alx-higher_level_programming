7a03d1785cbe1aff2626e347694d3599ceba5f76
Edimatic <ediomiudofia@gmail.com>
2024-04-13 11:08:37 -0700

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Lists all State objects and corresponding City objects contained in the DB
+"""
 import sys
 from relationship_state import Base, State
 from relationship_city import City
--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Lists all City objects from the database hbtn_0e_101_usa
+"""
 import sys
 from relationship_state import Base, State
 from relationship_city import City
